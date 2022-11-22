886d6fe4b753243c5fc0ccca7516eff51ed59481
simon <simon.schoening@web.de>
2022-11-22 16:31:14 +0100

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='h-screen bg-base-200'>
+      <Component {...pageProps} />
+    </div>)
+
 }
 
 export default MyApp
